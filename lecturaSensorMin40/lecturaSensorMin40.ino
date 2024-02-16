1b9b3e1701f5ecc49619c7ef0070ae06419e01a6
Luis N <89024247+LuisA01709621@users.noreply.github.com>
2024-02-15 19:06:21 -0600

    Create lecturaSensorMin40.ino

---
+++ lecturaSensorMin40/lecturaSensorMin40.ino
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <SerialStream.h>
+
+int main(){
+    using namespace LibSerial;
+
+    SerialStream serial_port;
+    serialport.Open("/dev/ttyUSB0"); //Remplazar con el puerto necesario
+
+    serial_port.SetBaudRate(SerialStreamBuf::Baud_P600); //Ajustar al sensor
+
+    if (!Serial_port.good()) { 
+        std::cerr << "Error al abrir el puerto serie" << std::endl;
+        return 1;
+    }
+
+    try {
+        while(true) {
+            float data[6];
+            serial_port.read(reinterpret_cast<char*>(data), sizeof(data));
+
+            for (i = 0; i < 6; ++i){
+                std::cout << data[i] << " ";
+            }
+            std::cout << std::endl;
+            usleep(1000000);
+        }
+    }
+    catch (const std::exeption& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        serial_port.Close();
+        return 1;
+    }
+    seria_port.Close();
+    return 0;
+}
